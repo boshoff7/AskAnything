91f9c2ebcd0b1cd2c1018185210193f21ecad0c3
boshoff7 <boshoff7@gmail.com>
2022-05-27 14:23:09 +0200

    Initial commit

--- AskAnything/ViewController.swift
+++ AskAnything/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let ballArray = [#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2")]
+    @IBOutlet weak var imageVeiw: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageVeiw.image = ballArray.randomElement()
+
     }
-
-
 }
-
